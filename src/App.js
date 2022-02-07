80c06365173465bf5e5a812cb3e59e2d298b293a
Sabau Adrian <sabau.adrian1995@gmail.com>
2022-02-07 15:57:56 +0200

    error resolve

--- src/App.js
+++ src/App.js
@@ -1,32 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Button from '@mui/material/Button';
-import SaveIcon from "@mui/icons-material/Save";
-import DeleteIcon from "@mui/icons-material/Delete";
-import {  ButtonGroup, Container, FormControlLabel, Grid, Paper, Typography } from '@mui/material';
-import { Checkbox } from '@mui/material';
-import AcUnitIcon from '@mui/icons-material/AcUnit';
-import AcUnitIconOutlined from '@mui/icons-material/AcUnit';
-import styled from "styled-components"
+import {  Container} from '@mui/material';
+// import styled from "styled-components"
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { green, purple } from '@mui/material/colors';
 import 'fontsource-roboto';
-import MenuIcon from '@mui/icons-material/Menu';
 import Header from './Header.js';
 import Home from './Home.js';
 
 
 
-const StyledButton = styled.button`
-color: white;
-font-size: 1em;
-margin: 1em;
-padding: 0.25em 1em;
-border: 2px solid palevioletred;
-border-radius: 3px;
-background: linear-gradient(45deg, #FE6B8B, #FF8E53)
-`;
+// const StyledButton = styled.button`
+// color: white;
+// font-size: 1em;
+// margin: 1em;
+// padding: 0.25em 1em;
+// border: 2px solid palevioletred;
+// border-radius: 3px;
+// background: linear-gradient(45deg, #FE6B8B, #FF8E53)
+// `;
 
 const theme = createTheme({
   typography: {
@@ -44,22 +36,6 @@ const theme = createTheme({
   },
 });
 
-function CheckBoxExample() {
-  const [checked, setChecked] = React.useState(false)
-  return(
-    <div>
-      <FormControlLabel control={<Checkbox
-        icon={<AcUnitIcon/>}
-        checkedIcon={<AcUnitIconOutlined />}
-        size ="large"
-        color = "primary"
-        checked={checked}
-        onChange={(e)=> setChecked(e.target.checked)}
-      />}label="Check this!" />
-      
-    </div>
-  )
-}
 function App() {
   return (
     <ThemeProvider theme={theme}>
--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { Box, Button, ButtonGroup, Container, Typography } from "@mui/material";
+import { Box, Button, Container, Typography } from "@mui/material";
 import React from "react";
 import FacebookIcon from '@mui/icons-material/Facebook';
 import InstagramIcon from '@mui/icons-material/Instagram';
@@ -7,78 +7,83 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 import Typical from 'react-typical';
 import profileImg from '../src/images/Linkedin.png';
 import './Home.css';
-import { ThemeProvider, createTheme, responsiveFontSizes} from '@mui/material/styles';
+import { ThemeProvider, createTheme,} from '@mui/material/styles';
 
 
 const theme = createTheme({
-    
+
     palette: {
-      primary: {
-        main: "#F0F0F0",
-      },
-      secondary: {
-        main: "#F1D00A",
-      },
+        primary: {
+            main: "#F0F0F0",
+        },
+        secondary: {
+            main: "#F1D00A",
+        },
     },
-  });
+});
 
 theme.typography.h3 = {
     fontSize: '2.3rem',
     pl: '5px',
     '@media (min-width:600px)': {
-      fontSize: '2.3rem',
+        fontSize: '2.3rem',
     },
     [theme.breakpoints.up('md')]: {
         fontSize: '3rem'
     },
     [theme.breakpoints.up('lg')]: {
-      fontSize: '5rem',
+        fontSize: '5rem',
     },
-  };
+};
 
 const Home = () => {
-    return(
+    return (
         <ThemeProvider theme={theme}>
             <Container disableGutters maxWidth={false}>
                 <Box
-                className="box-container"
-                sx={{
-                    display:'flex',
-                    paddingTop: 2,
-                    justifyContent:'center',
-                    backgroundColor: '#3E497A',
-                    height: '100vh',
-                
-                    
-                }}>
-                <Box 
-                className="text-container"
-                sx={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                   
-                    
-                    alignItems: 'center',
-                    justifyContent: 'center'
-                    
-                }}>
-                    <Typography
-                    className="text"
-                    display='flex'
-                    variant='h3'>
-                        Hello, I'm <Typography variant='h3' pl='15px' color="#F1D00A">Adrian</Typography>
-                    </Typography>
-                    <p>
-                    I'm a {' '}<Typical  
-                    steps={['Front-End Developer 💻', 1000,
-                            'React Enthusiast 😎', 1000,
-                            'Css & HTML Wizard 🧙‍♂️', 1000
-                        ]}
-                    loop={Infinity}
-                    wrapper="b"/>
-                    
-                    </p>
-                    <Box>
+                    className="box-container"
+                    sx={{
+                        display: 'flex',
+                        paddingTop: 2,
+                        justifyContent: 'center',
+                        backgroundColor: '#3E497A',
+                        height: '100vh',
+
+
+                    }}>
+                    <Box
+                        className="text-container"
+                        sx={{
+                            display: 'flex',
+                            flexDirection: 'column',
+
+
+                            alignItems: 'center',
+                            justifyContent: 'center'
+
+                        }}>
+                        <Box
+                            sx={{
+                                display: 'flex'
+                            }}>
+                            <Typography
+                                className="text"
+                                display='flex'
+                                variant='h3'>
+                                Hello, I'm
+                            </Typography>
+                            <Typography variant='h3' pl='15px' color="#F1D00A">Adrian</Typography>
+                        </Box>
+                        <p>
+                            I'm a {' '}<Typical
+                                steps={['Front-End Developer 💻', 1000,
+                                    'React Enthusiast 😎', 1000,
+                                    'Css & HTML Wizard 🧙‍♂️', 1000,
+                                ]}
+                                loop={Infinity}
+                                wrapper="b" />
+                        </p>
+                        <Box>
                             <Button href='https://www.facebook.com/sabau.adrian.75'>
                                 <FacebookIcon />
                             </Button>
@@ -91,19 +96,19 @@ const Home = () => {
                             <Button href='https://github.com/Sabau-Adrian'>
                                 <GitHubIcon />
                             </Button>
+                        </Box>
+
                     </Box>
-                    
-                </Box>
-                <Box
-                className="img-container"
-                sx={{
-                    display:'flex',
-                    alignItems: 'center',
-                    justifyContent: 'flex-start'
-                    
-                }}>
-                    <img alt='Personal-img' src={profileImg}></img>
-                </Box>
+                    <Box
+                        className="img-container"
+                        sx={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'flex-start'
+
+                        }}>
+                        <img alt='Personal-img' src={profileImg}></img>
+                    </Box>
                 </Box>
             </Container>
         </ThemeProvider>
